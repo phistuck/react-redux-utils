7e0ff239663ed4410155a4c0129e781055e29c69
Dmitry Nutels <dnutels@gmail.com>
2016-03-13 22:55:13 +0200

    0.0.7

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 import _createActionTypes from './createActionTypes';
 export { _createActionTypes as createActionTypes };
+
+import _createActionCreator from './createActionCreator';
+export { _createActionCreator as createActionCreator };
